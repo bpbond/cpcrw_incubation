c77c3557646c4c13fe169c11b2caf8cb26eabcf1
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-12-13 16:47:39 -0500

    Script now handles both gases, not just CO2

--- R/5-highvar.R
+++ R/5-highvar.R
@@ -12,18 +12,84 @@ RAWDATA      <- file.path(OUTPUT_DIR, "rawdata_samples.csv.gz")  # output from s
 TREATMENT_SUMMARYDATA  <- file.path(OUTPUT_DIR, "treatment_summary.csv")  # output from script 3
 
 
+
+investigate <- function(smry, summarydata, rawdata, 
+                        gas, gasvar, gasvar_sd = paste0(gasvar, "_sd"), 
+                        gasvar_cv = paste0(gasvar, "_cv")) {
+  
+  smry$ymin <- smry[,gasvar] - smry[,gasvar_sd]
+  smry$ymax <- smry[,gasvar] + smry[,gasvar_sd]
+  p <- ggplot(smry, aes_string("Date", gasvar, color = gasvar_cv)) + 
+    geom_point() +
+    facet_grid(Temperature ~ Treatment) + 
+    geom_errorbar(aes(ymin = ymin, ymax = ymax)) + 
+    scale_color_continuous(guide = FALSE) +
+    geom_text(aes(y = ymax, label = Num), vjust = -0.1, hjust = 0.1)
+  save_plot(paste0("summary_", gas), p = p)
+  
+  ok <- TRUE
+  while(ok) {
+    
+    printlog("Looking at gas", gas)
+    print(p)
+    
+    n <- readline("Which do you want to investigate (0 to exit)? ") %>% as.numeric()
+    
+    if(n %in% seq_len(nrow(smry))) {
+      printlog(smry[n,])
+      
+      samplenums <- smry[n, "samplenums"] %>% strsplit(" ") %>% unlist %>% as.numeric()
+      
+      printlog("Samples from the summary dataset:")
+      sd_gas <- subset(summarydata, samplenum %in% samplenums)
+      print(sd_gas)
+      
+      p_closeup <- ggplot(sd_gas, aes_string("DATETIME", gasvar, fill="Core")) + 
+        geom_bar(stat='identity') + 
+        ggtitle(paste("Closeup look at number", n, "on incday", unique(sd_gas$incday))) +
+        geom_text(aes(label = samplenum), vjust = -0.5)
+      print(p_closeup)
+      save_plot(paste0(gas, "_closeup_", n))
+      
+      p_distribution <- ggplot(subset(summarydata, Treatment %in% sd_gas$Treatment & Temperature %in% sd_gas$Temperature), aes_string(x = gasvar)) + 
+        geom_density() + 
+        facet_grid(Temperature ~ Treatment, scales = "free") + 
+        geom_vline(data=sd_gas, aes_string(xintercept = gasvar, color="Core"), linetype = 2) +
+        ggtitle(paste("Distribution look at number", n))
+      print(p_distribution)
+      save_plot(paste0(gas, "_distribution_", n))
+      
+      
+      rd_gas <- subset(rawdata, samplenum %in% samplenums)
+      p_rawcloseup <- ggplot(rd_gas, aes_string("elapsed_seconds", paste0(gas, "_dry"), color="samplenum")) +
+        geom_line() +
+        ggtitle(paste("Raw closeup look at number", n, "on", unique(rd_gas$DATE)))
+      print(p_rawcloseup)
+      save_plot(paste0(gas, "_closeup_raw_", n))
+      
+    } else {
+      ok <- FALSE
+    }
+  } # while
+  
+}
+
 # Main ===========================================================================
 
 openlog(file.path(outputdir(), paste0(SCRIPTNAME, ".log.txt")), sink = TRUE) # open log
 
 printlog("Welcome to", SCRIPTNAME)
 
+printlog("Reading in raw data...")
+#rawdata <- read_csv(RAWDATA)
 printlog("Reading in summary data...")
 summarydata <- read_csv(SUMMARYDATA)
 summarydata$DATETIME <- ymd_hms(summarydata$DATETIME)
 printlog("Reading in treatment summaries...")
 tsummary <- read_csv(TREATMENT_SUMMARYDATA)
 tsummary$Date <- ymd(tsummary$Date)
+
+# ------------------- CO2
 
 printlog("Looking at high-variability CO2...")
 tsummary %>%
@@ -38,57 +104,28 @@ cv_cutoff <- 0.9
 smry_co2$Num <- 1:nrow(smry_co2)
 smry_co2$Num[smry_co2$CO2_ppm_s_sd / smry_co2$CO2_ppm_s < cv_cutoff] <- NA
 smry_co2$Num[1:10] <- 1:10  # largest 10 always get labeled
+smry_co2$CO2_ppm_s_cv <- with(smry_co2, CO2_ppm_s_sd / CO2_ppm_s)
+investigate(smry = smry_co2, summarydata = summarydata, rawdata = rawdata, 
+            gas = "CO2", gasvar = "CO2_ppm_s")
 
-p <- ggplot(smry_co2, aes(Date, CO2_ppm_s, color = CO2_ppm_s_sd/CO2_ppm_s)) + geom_point() +
-  facet_grid(Temperature ~ Treatment) + 
-  geom_errorbar(aes(ymin = CO2_ppm_s - CO2_ppm_s_sd, ymax = CO2_ppm_s + CO2_ppm_s_sd)) + 
-  scale_color_continuous(guide = FALSE) +
-  geom_text(aes(y = CO2_ppm_s + CO2_ppm_s_sd, label = Num), vjust = -0.1, hjust = 0.1)
-save_plot("summary_co2", p = p)
+# ------------------- CH4
 
-ok <- TRUE
-while(ok) {
-  
-  print(p)
-  
-  n <- readline("Which do you want to investigate (0 to exit)? ") %>% as.numeric()
-  
-  if(n %in% seq_len(nrow(smry_co2))) {
-    printlog(smry_co2[n,])
-    
-    samplenums <- smry_co2[n, "samplenums"] %>% strsplit(" ") %>% unlist %>% as.numeric()
-    
-    printlog("Samples from the summary dataset:")
-    sd_co2 <- subset(summarydata, samplenum %in% samplenums)
-    print(sd_co2)
-    
-    p_closeup <- ggplot(sd_co2, aes(DATETIME, CO2_ppm_s, fill=Core)) + 
-      geom_bar(stat='identity') + 
-      ggtitle(paste("Closeup look at number", n, "on incday", unique(sd_co2$incday))) +
-      geom_text(aes(label = samplenum), vjust = -0.5)
-    print(p_closeup)
-    save_plot(paste0("closeup_", n))
-    
-    p_distribution <- ggplot(subset(summarydata, Treatment %in% sd_co2$Treatment & Temperature %in% sd_co2$Temperature), aes(x=CO2_ppm_s)) + 
-      geom_density() + 
-      facet_grid(Temperature ~ Treatment, scales="free") + 
-      geom_vline(data=sd_co2, aes(xintercept = CO2_ppm_s, color=Core), linetype=2) +
-      ggtitle(paste("Distribution look at number", n))
-    print(p_distribution)
-    save_plot(paste0("distribution_", n))
-    
-      
-      rd_co2 <- subset(rawdata, samplenum %in% samplenums)
-    p_rawcloseup <- ggplot(rd_co2, aes(elapsed_seconds, CO2_dry, color=samplenum)) +
-      geom_line() +
-      ggtitle(paste("Raw closeup look at number", n, "on", unique(rd_co2$DATE)))
-    print(p_rawcloseup)
-    save_plot(paste0("closeup_raw_", n))
-    
-  } else {
-    ok <- FALSE
-  }
-} # while
+printlog("Looking at high-variability CH4...")
+tsummary %>%
+  filter(!is.na(CH4_ppb_s_sd), Treatment != "Ambient") %>% 
+  arrange(desc(CH4_ppb_s_sd)) %>%
+  select(Date, Treatment, Temperature, CH4_ppb_s_sd, CH4_ppb_s, incday, samplenums) ->
+  smry_ch4
+
+save_data(smry_ch4)
+
+cv_cutoff <- 0.9
+smry_ch4$Num <- 1:nrow(smry_ch4)
+smry_ch4$Num[smry_ch4$CH4_ppb_s_sd / smry_ch4$CH4_ppb_s < cv_cutoff] <- NA
+smry_ch4$Num[1:10] <- 1:10  # largest 10 always get labeled
+smry_ch4$CH4_ppb_s_cv <- with(smry_ch4, CH4_ppb_s_sd / CH4_ppb_s)
+investigate(smry = smry_ch4, summarydata = summarydata, rawdata = rawdata, 
+            gas = "CH4", gasvar = "CH4_ppb_s")
 
 # Done! 
 
